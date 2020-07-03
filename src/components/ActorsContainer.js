25ea6bc412f569c17209a494ca975cae4e71f218
Michelle <[mlee092@gmail.com]>
2020-07-03 17:31:07 -0400

    Actors Containers

---
+++ src/components/ActorsContainer.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import {ActorList} from '../data/actors';
+import Actor from './actor';
+import Actors from './actors';
+import { Route} from 'react-router-dom';
+ 
+//const ActorsContainer
+
+export default ActorsContainer;
