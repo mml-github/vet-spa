003f8b8a5eeb5a788ac731a1cc8a8258ed3736a9
Michelle <[mlee092@gmail.com]>
2020-07-15 11:22:00 -0400

    actress.js

--- src/components/Actress.js
+++ src/components/Actress.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {ActressList} from '../data/actress';
 import { Link } from 'react-router-dom';
 
+const Actress = (props) => {
+    let actress = ActressList.map((actress) => {
+      return (
+        <div className="actor-container">
+          <Link to={`/actors/${actress.url}`}><div className="actor-image" style={{ backgroundImage: "url(" + actress.img_src + ")" }}></div></Link>
+          <h3>{actress.name}</h3>
+        </div>
+      );
+    });
 
+    return (
+
+    );
+}
 
 export default Actress;
